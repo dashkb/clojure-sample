bfb96bfee5c871dc8b7895fe71e0dc6810117ba7
Kyle Brett <kyle@kylebrett.com>
2016-02-16 11:50:20 -0800

    add pg

--- project.clj
+++ project.clj
@@ -2,5 +2,8 @@
   :description "Hello World Clojure Web App"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.1"]
-                 [ring/ring-jetty-adapter "1.1.2"]]
+                 [ring/ring-jetty-adapter "1.1.2"]
+                 [org.clojure/java.jdbc "0.4.1"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]
+                 ]
   :main ^:skip-aot sample.app)

76d8ef711c90d1542bc6731239768d330213c4b5
Kyle Brett <kyle@kylebrett.com>
2016-02-16 12:07:25 -0800

    Things

--- src/sample/app.clj
+++ src/sample/app.clj
@@ -1,11 +1,13 @@
 (ns sample.app
   (:require [ring.adapter.jetty :as jetty]
-            [compojure.core :refer [defroutes GET]]))
+            [compojure.core :refer [defroutes GET]]
+            [clojure.java.jdbc :as sql]))
 
 (defroutes handler
   (GET "/" []
        {:headers {"Content-type" "text/plain; charset=UTF-8"}
-        :body "Hello world!"}))
+        :body (sql/query (System/getenv "DATABASE_URL")
+                         "select * from users limit 5")}))
 
 (defn -main []
   (jetty/run-jetty handler
